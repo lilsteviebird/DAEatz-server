afe36b5422bc42be5d9c32e933c5aa321231247d
lilsteviebird <stevenli880@gmail.com>
2018-08-20 21:37:52 +0100

    just added to test tdate

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.json());
 
 let now = new Date();
 
-var tdate = date.format(now, 'YYYY-MM-DD');
+// var tdate = date.format(now, 'YYYY-MM-DD');
+
+var tdate = date.format(now, '2018-08-21');
 
 
 
